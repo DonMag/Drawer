5e81522238a34a70725b3e7306ee48fa5fcb1223
DMag <dmagnusson@thinmanager.com>
2016-06-01 15:16:30 -0400

    Logical
    use more logical / understandable constraint manipulation; hide drawer
    when closed

--- Drawer/ViewController.swift
+++ Drawer/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-
+// helper
 extension UIView {
 	func animateConstraintWithDuration(duration: NSTimeInterval = 0.5, delay: NSTimeInterval = 0.0, options: UIViewAnimationOptions = [], completion: ((Bool) -> Void)? = nil) {
 		UIView.animateWithDuration(duration, delay:delay, options:options, animations: { [weak self] in
@@ -21,6 +21,7 @@ extension UIView {
 class ViewController: UIViewController {
 
 	var gDrawerView: InfoDrawerView?
+	var gBottomConstraint: NSLayoutConstraint?
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
@@ -40,77 +41,77 @@ class ViewController: UIViewController {
 		let drawerView = InfoDrawerView()
 		drawerView.backgroundColor = UIColor.greenColor()
 		
+		// the "drawer" needs to be hidden when "closed" - otherwise it is partially visible when the device is rotated
+		drawerView.hidden = true
+		
 		// important - views created via code are set to TRUE
 		drawerView.translatesAutoresizingMaskIntoConstraints = false
+		
+		// add new view to current view (could also be added to a view higher up in the hierarchy)
+		self.view.addSubview(drawerView)
 		
 		// array of views (just the one, in this case)
 		var views = [String:AnyObject]()
 		views["dview"] = drawerView
 		
-		// add new view to current view (could also be added to a view higher up in the hierarchy)
-		self.view.addSubview(drawerView)
-		
-		// array for constraints
-		var dViewConstraints = [NSLayoutConstraint]()
-		
 		var vflString = ""
-		
-		// vertical layout: 70 pixels height, pinned to the bottom of the superview
-		// we want it "closed" to begin with, so put it 70 pixels below the bottom
-		vflString = "V:[dview(70)]-(-70)-|"
-		
-		let verticalConstraint = NSLayoutConstraint.constraintsWithVisualFormat(vflString, options: [], metrics: nil, views: views)
 		
 		// horizontal layout: pinned to left and right edges of the superview
 		vflString = "H:|[dview]|"
 		
 		let horizontalConstraint = NSLayoutConstraint.constraintsWithVisualFormat(vflString, options: [], metrics: nil, views: views)
 		
-		dViewConstraints += horizontalConstraint + verticalConstraint
+		// vertical layout: 70 pixels height
+		vflString = "V:[dview(70)]"
+		
+		let verticalConstraint = NSLayoutConstraint.constraintsWithVisualFormat(vflString, options: [], metrics: nil, views: views)
+
+		// vertical layout: position of "drawer" view - this will become our modifiable constraint used to show/hide the drawer
+		// set to 70 pixels below the bottom of the view, so we can "slide it up"
+		let botCon = NSLayoutConstraint(
+			item: drawerView,
+			attribute: .Bottom,
+			relatedBy: NSLayoutRelation.Equal,
+			toItem: self.view,
+			attribute: .Bottom,
+			multiplier: 1,
+			constant: 70
+		)
 		
 		// add the constraints
-		self.view.addConstraints(dViewConstraints)
+		self.view.addConstraints(horizontalConstraint)
+		self.view.addConstraints(verticalConstraint)
+		self.view.addConstraint(botCon)
 		
+		self.gBottomConstraint = botCon
 		self.gDrawerView = drawerView
 	
 	}
 
 	@IBAction func bTapped(sender: AnyObject) {
 
-		self.showHideDrawer()
-		
-	}
-	
-	func showHideDrawer() {
-		
-		guard let bCon = self.findBottomConstraint(self.gDrawerView) else {
-			return
-		}
-		
-		if bCon.constant == 0 {
-			bCon.constant = -70
+		if gBottomConstraint?.constant == 0 {
+			// the drawer is "open", so slide it down and set to hidden
+			
+			gBottomConstraint?.constant = 70
+			self.view.animateConstraintWithDuration(0.25, delay: 0.0, options: [],
+				completion:
+				{(finished:Bool)->Void in self.gDrawerView?.hidden = true }
+			)
+			
 		} else {
-			bCon.constant = 0
+			// the drawer is "closed", so show it and slide it up
+			
+			gDrawerView?.hidden = false
+			gBottomConstraint?.constant = 0
+			self.view.animateConstraintWithDuration(0.25, delay: 0.0, options: [],
+				completion: nil
+			)
+			
 		}
 		
-		self.view.animateConstraintWithDuration(0.25)
+		return
 		
-	}
-	
-	func findBottomConstraint(targView: UIView?) -> NSLayoutConstraint? {
-		var retCon: NSLayoutConstraint?
-		
-		let fArray = self.view.constraints.filter{
-			(($0.firstAttribute == NSLayoutAttribute.Bottom || $0.secondAttribute == NSLayoutAttribute.Bottom)
-			&&
-			($0.firstItem as? UIView == targView || $0.secondItem as? UIView == targView))
-		}
-		
-		if fArray.count == 1 {
-			retCon = fArray[0]
-		}
-		
-		return retCon
 	}
 	
 }
